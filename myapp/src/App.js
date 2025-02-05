4405431852006e2fe1e623c948290093a231203d
tarun aggarwal <tarun.agarwal@Quadrafort.com>
2025-02-05 17:10:21 +0530

    docker

---
+++ myapp/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const a = 1
+  const b =2
+  let c = (a+b);
+  return (
+<>
+<Text>{c}</Text>
+</>
+  );
+}
+
+export default App;
